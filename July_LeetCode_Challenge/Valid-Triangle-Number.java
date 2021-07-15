4a4fc00d674d151a76997502a81ec6401a94e75d
Riya Gupta <61061825+gupta-riya@users.noreply.github.com>
2021-07-15 22:31:12 +0530

    completed

---
+++ July_LeetCode_Challenge/Valid-Triangle-Number.java
@@ -0,0 +1,40 @@
+// Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+
+ class Solution {
+    public int triangleNumber(int[] nums) {
+        
+        Arrays.sort(nums);
+        
+        
+        int count = 0;
+        
+        for(int i = nums.length-1 ; i >= 2 ; i--)
+        {
+            int l = 0, r = i-1;
+            
+            while(l < r)
+            {
+                if(nums[l] + nums[r] > nums[i])
+                    { 
+                        count += r - l;
+                        r--;
+                     }
+                else
+                {
+                    l++;
+                }
+                
+                
+            }
+            
+          
+            
+            
+        }
+        
+          return count;
+        
+        
+        
+    }
+}
